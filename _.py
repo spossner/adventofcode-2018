57bbf2217af54337cc34f8b6ef6d418789d460e2
spossner <github@possner.de>
2022-01-04 15:00:00 +0100

    rename the template to find it at the bottom of the file list in IDE

---
+++ _.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import math
+import operator
+import re
+from collections import deque, defaultdict, namedtuple, Counter
+from functools import total_ordering, reduce
+from itertools import permutations, zip_longest, count
+import bisect
+
+import networkx as nx
+import pygame as pg
+import numpy as np
+from aoc import Point, Rect, NORTH, rot_ccw, rot_cw, translate
+
+
+class Solution:
+    def __init__(self, data, modified=False, do_strip=False, do_splitlines=True, split_char=None):
+        if data and do_strip:
+            data = data.strip()
+        if data and do_splitlines:
+            data = data.splitlines()
+        if data and split_char:
+            if split_char == '':
+                data = [list(row) for row in data] if do_splitlines else list(data)
+            else:
+                data = [row.split(split_char) for row in data] if do_splitlines else data.split(split_char)
+        self.data = data
+        self.modified = modified
+
+    def solve(self):
+        pass
+
+
+if __name__ == '__main__':
+    script = os.path.splitext(os.path.basename(sys.argv[0]))[0]
+    if '-' in script:
+        script = script.split('-')[0]
+
+    DEV = True
+    PART2 = False
+    STRIP = True
+    SPLIT_LINES = True
+    SPLIT_CHAR = None
+
+    with open(f'{script}{"-dev" if DEV else ""}.txt') as f:
+        s = Solution(f.read(), PART2, STRIP, SPLIT_LINES, SPLIT_CHAR)
+        print(s.solve())
